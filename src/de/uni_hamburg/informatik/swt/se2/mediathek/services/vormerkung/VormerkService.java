b20e07c96ddd074f5d8c8a5f8f60fb9fb30be0ea
suushie_maniac <suushiemaniac@gmail.com>
2016-05-30 21:34:12 +0200

    Actually implement VormerkService

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/services/vormerkung/VormerkService.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/services/vormerkung/VormerkService.java
@@ -2,15 +2,16 @@ package de.uni_hamburg.informatik.swt.se2.mediathek.services.vormerkung;
 
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.Kunde;
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.ObservableService;
 
 import java.util.List;
 import java.util.Queue;
 
-public interface VormerkService
+public interface VormerkService extends ObservableService
 {
     void merkeVor(Kunde kunde, List<Medium> medien);
 
-    boolean istVormerkenMoeglich(Kunde kunde, List<Medium> medien);
+    void entferneVormerkung(Kunde kunde, List<Medium> medien);
 
     Queue<Kunde> getVormerkerFuer(Medium medium);
 
--- src/de/uni_hamburg/informatik/swt/se2/mediathek/services/vormerkung/VormerkServiceImpl.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/services/vormerkung/VormerkServiceImpl.java
@@ -2,40 +2,79 @@ package de.uni_hamburg.informatik.swt.se2.mediathek.services.vormerkung;
 
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.Kunde;
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+import de.uni_hamburg.informatik.swt.se2.mediathek.services.AbstractObservableService;
 
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-public class VormerkServiceImpl implements VormerkService
+public class VormerkServiceImpl extends AbstractObservableService
+        implements VormerkService
 {
-    @Override
-    public void merkeVor(Kunde kunde, List<Medium> medien)
-    {
+    private Map<Medium, Queue<Kunde>> _vormerkungen;
 
+    public VormerkServiceImpl()
+    {
+        _vormerkungen = new HashMap<>();
     }
 
     @Override
-    public boolean istVormerkenMoeglich(Kunde kunde,
-            List<Medium> medien)
+    public void merkeVor(Kunde kunde, List<Medium> medien)
     {
-        return false;
+        for (Medium medium : medien)
+        {
+            Queue<Kunde> vormerker = _vormerkungen.getOrDefault(medium, new LinkedList<>());
+            if (vormerker.size() < 3)
+            {
+                vormerker.add(kunde);
+                _vormerkungen.put(medium, vormerker);
+            }
+        }
+
+        informiereUeberAenderung();
+    }
+
+    @Override
+    public void entferneVormerkung(Kunde kunde, List<Medium> medien)
+    {
+        for (Medium medium : medien)
+        {
+            Queue<Kunde> vormerker = _vormerkungen.get(medium);
+            if (vormerker != null && vormerker.contains(kunde))
+            {
+                vormerker.remove(kunde);
+                _vormerkungen.put(medium, vormerker);
+            }
+        }
+
+        informiereUeberAenderung();
     }
 
     @Override
     public Queue<Kunde> getVormerkerFuer(Medium medium)
     {
-        return null;
+        return _vormerkungen.getOrDefault(medium, new LinkedList<>());
     }
 
     @Override
     public List<Medium> getVorgemerkteMedienFuer(Kunde kunde)
     {
-        return null;
+        List<Medium> vorgemerkteMedien = new ArrayList<>();
+
+        for (Medium medium : _vormerkungen.keySet())
+        {
+            if (_vormerkungen.get(medium).contains(kunde))
+            {
+                vorgemerkteMedien.add(medium);
+            }
+        }
+
+        return vorgemerkteMedien;
     }
 
     @Override
     public boolean istVorgemerkt(Medium medium)
     {
-        return false;
+        Queue<Kunde> vormerker = _vormerkungen.get(medium);
+
+        return vormerker != null && vormerker.size() > 0;
     }
 }
